a256ef677f4f80f61143e43ebcf512cb6bcbeb0a
t <onlyyouandty@gmail.com>
2015-08-17 14:00:33 +0100

    Finished learnyoumongo

--- learnyoumongo/aggregate.js
+++ learnyoumongo/aggregate.js
@@ -1,15 +1,16 @@
 var mongo = require('mongodb').MongoClient;
-var age = process.argv[2];
 var url = 'mongodb://localhost:27017/learnyoumongo';
 
+var match = { $match: { size: process.argv[2] } };
+var group = { $group: { _id: 'total', total: { $avg: '$price' } } };
+
 mongo.connect(url, function(err, db) {
+	if (err) throw err
 	var collection = db.collection('prices')
-	collection.aggregate({
-		age: {
-			$gt: +age
-		}
-	}, function(err, count) {
-		console.log(count)
-		db.close()
-	})
+	collection.aggregate([ match, group
+		]).toArray(function(err, results) {
+			var o = results[0]
+			console.log(Number(o.total).toFixed(2))
+			db.close()
+		})
 })
